2c29c9fa117052fa16714ce740b243d487d3f3a2
Shakall1944 <lukas.kalan@gmail.com>
2023-01-25 21:29:28 +0100

    Data types and variables 2

--- ace_game.cpp
+++ ace_game.cpp
@@ -7,7 +7,16 @@
 #include "raylib.h"
 int main()
 {
+    // Variables of program
     int width;
+    float root_beer;
+    double cheese_burger;
+    bool shouldHaveLunch;
+    // Declaration of variables
     width = 350;
+    root_beer = 1.99;
+    cheese_burger = 5.99;
+    shouldHaveLunch = true;
+    //Running raylib
     InitWindow(width, 300, "Lukas's Window");
 }
