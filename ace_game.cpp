e90203270be5dd1dea131e22dc7157c7f383dc96
Shakall1944 <lukas.kalan@gmail.com>
2023-01-25 21:57:28 +0100

    Comparison operatos

--- ace_game.cpp
+++ ace_game.cpp
@@ -8,11 +8,20 @@
 int main()
 {
     // Variables of program
-    int width;
-    float root_beer = 1.99;
-    double cheese_burger{5.99};
-    bool shouldHaveLunch{};
+    //false
+    bool equal{ 4 == 9 };
+    //true
+    bool notEqual{ 4 != 9 };
+    //true
+    bool less{ 4 < 9 };
+    //false
+    bool greater{ 4 > 9 };
+    //true
+    bool lessEqual{ 4 <= 9 };
+    //false
+    bool greaterEqual{ 4 >= 9 };
     // Declaration of variables
+    int width;
     width = 350;
     //Running raylib
     InitWindow(width, 300, "Lukas's Window");
