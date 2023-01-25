cfd5fd513ffbcc5c61c0d449683bf4e6b6036d63
Shakall1944 <lukas.kalan@gmail.com>
2023-01-25 21:42:43 +0100

    Debugging

--- ace_game.cpp
+++ ace_game.cpp
@@ -9,14 +9,11 @@ int main()
 {
     // Variables of program
     int width;
-    float root_beer;
-    double cheese_burger;
-    bool shouldHaveLunch;
+    float root_beer = 1.99;
+    double cheese_burger{5.99};
+    bool shouldHaveLunch{};
     // Declaration of variables
     width = 350;
-    root_beer = 1.99;
-    cheese_burger = 5.99;
-    shouldHaveLunch = true;
     //Running raylib
     InitWindow(width, 300, "Lukas's Window");
 }
