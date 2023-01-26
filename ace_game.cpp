ed4bf0b40d165196ca5462501396d24286dd1fbf
Shakall1944 <lukas.kalan@gmail.com>
2023-01-26 18:25:06 +0100

    Loops

--- ace_game.cpp
+++ ace_game.cpp
@@ -7,22 +7,15 @@
 #include "raylib.h"
 int main()
 {
-    // Variables of program
-    //false
-    bool equal{ 4 == 9 };
-    //true
-    bool notEqual{ 4 != 9 };
-    //true
-    bool less{ 4 < 9 };
-    //false
-    bool greater{ 4 > 9 };
-    //true
-    bool lessEqual{ 4 <= 9 };
-    //false
-    bool greaterEqual{ 4 >= 9 };
     // Declaration of variables
     int width;
     width = 350;
     //Running raylib
     InitWindow(width, 300, "Lukas's Window");
+    
+    while (true)
+    {
+        /* code */
+    }
+    
 }
