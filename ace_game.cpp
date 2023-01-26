a24b9cd53d11b39bba4d887bf2af9119249a7256
Shakall1944 <lukas.kalan@gmail.com>
2023-01-26 19:28:22 +0100

    Logical AND operator

--- ace_game.cpp
+++ ace_game.cpp
@@ -7,17 +7,46 @@
 #include "raylib.h"
 int main()
 {
-    // Declaration of variables
-    int width;
-    width = 350;
+    // Declaration of variables - window dimensions
+    int width{350};
+    int height{200};
     //Running raylib
-    InitWindow(width, 300, "Lukas's Window");
+    InitWindow(width, height, "Lukas's Window");
+
+    //circle coordinates
+    int circle_x{175};
+    int circle_y{100};
     
+    SetTargetFPS(60);
     while ( WindowShouldClose() == false )
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        DrawCircle(175, 150, 25, BLUE);
+
+        //Game login begins!
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+
+        if (IsKeyDown(KEY_D) && circle_x < 350 )
+        {
+            circle_x = circle_x + 10;
+        }
+        
+        if (IsKeyDown(KEY_A) && circle_x > 0 )
+        {
+            circle_x = circle_x - 10;
+        }
+        
+        if (IsKeyDown(KEY_W))
+        {
+            circle_y = circle_y - 10;
+        }
+
+        if (IsKeyDown(KEY_S))
+        {
+            circle_y = circle_y + 10;
+        }
+        
+        //Game logic ends!
         EndDrawing();
     }
     
