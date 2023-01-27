eab34c3f3177327b8de91a5c737833cfd829e7e7
Shakall1944 <58850495+Shakall1944@users.noreply.github.com>
2023-01-27 20:30:25 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+* hello.cpp
+* Prints out my name, home town,
+* and my favorite hobby.
+* by Stephen
+*/
+#include <cstdio>
+int main()
+{
+    // Prints out my name
+    printf("Hello, my name is Stephen.\n");
+    // Prints my home town
+    printf("I'm from Albuquerque, New Mexico.\n");
+    // There is no beach in New Mexico!
+    printf("I enjoy long walks on the beach.\n");
+}
