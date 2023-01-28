114843fd638d5706b5a6b0f469d8376f20e722da
Shakall1944 <lukas.kalan@gmail.com>
2023-01-28 18:55:38 +0100

    Delta time

--- dasher.cpp
+++ dasher.cpp
@@ -13,20 +13,29 @@ int main()
     InitWindow(windowsWidth, windowsHeight, "Lukas's game!");
 
     // accelaration due to gravity (pixel/frame)/frame + variable for jump
-    const int gravity{1};
-    const int jumpVel{-22};
+    //const int gravity{1};
+    //accelaration due to gravity (pixel/s)/s
+    const int gravity{1000};
+    //const int jumpVel{-22};
+    //jump velocity (pixels/second)
+    const int jumpVel{-600};
     bool isInAir{false};
 
-    //
+    // set up the variables for scarpy textures - raylib parameters
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     Rectangle scarfyRec;
+    scarfyRec.width = scarfy.width/6;
+    scarfyRec.height = scarfy.height;
+    scarfyRec.x= 0;
+    scarfyRec.y= 0;
     Vector2 scarfyPos;
+    scarfyPos.x = windowsWidth/2 - scarfyRec.width/2;
+    scarfyPos.y = windowsHeight - scarfyRec.height;
 
     // rectangle dimenstions
-    const int width(50);
-    const int height(80);
-
-    int posY(windowsHeight - height);
+    //const int width(50);
+    //const int height(80);
+    //int posY(windowsHeight - height);
     int velocity{0};
    
 
@@ -37,9 +46,11 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+        // delta time variable - time since last frame
+        const float dT{GetFrameTime()};
 
         // ground check
-        if (posY >= windowsHeight - height)
+        if (scarfyPos.y >= windowsHeight - scarfyRec.height)
         {
             // rectangle is on the ground
             velocity = 0;
@@ -48,7 +59,7 @@ int main()
         else 
         {
         // rectangle is in the air
-            velocity += gravity;
+            velocity += gravity * dT;
             isInAir = true;
 
         }
@@ -58,13 +69,14 @@ int main()
             velocity += jumpVel;
         }
         //update Y postion
-        posY += velocity;
+        scarfyPos.y += (velocity * dT);
     
-        DrawRectangle(windowsWidth/2, posY, width, height, BLUE);
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
         
         //Stop drawing
         EndDrawing();
     }
+    UnloadTexture(scarfy);
     CloseWindow();
     
 }
