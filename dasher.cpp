4760c3e3d613e6ae17e2393dc8c3b8f6854be0f6
Shakall1944 <lukas.kalan@gmail.com>
2023-01-28 15:27:10 +0100

    Velocity and jumping

--- dasher.cpp
+++ dasher.cpp
@@ -1,19 +1,43 @@
-#include "raylib.h";
+/* This is Dasher game
+ * Made by Lukas Kalan
+ *
+ */
+
+#include "raylib.h"
 int main()
 {
     //Set dimension of window
-    int width{800};
-    int height{450};
-    InitWindow(width, height, "Lukas's game'");
+    const int windowsWidth{800};
+    const int windowsHeight{450};
+    //
+    InitWindow(windowsWidth, windowsHeight, "Lukas's game!");
+
+    // rectangle dimenstions
+    const int width(50);
+    const int height(80);
+
+    int posY(windowsHeight - height);
+    int velocity{0};
 
     SetTargetFPS(60);
-    while (true)
+    while (!WindowShouldClose())
     {
+        // Start drawing
         BeginDrawing();
         ClearBackground(WHITE);
+
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
+        
+        posY += velocity;
+
+        DrawRectangle(windowsWidth/2, posY, width, height, BLUE);
+        
+        //Stop drawing
         EndDrawing();
     }
+    CloseWindow();
     
-
-
 }
