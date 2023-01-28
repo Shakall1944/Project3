008ff2a099b16c10a9beed0bddb77506a7fd9d3c
Shakall1944 <lukas.kalan@gmail.com>
2023-01-28 16:07:48 +0100

    Gravity and Jumping

--- dasher.cpp
+++ dasher.cpp
@@ -12,12 +12,18 @@ int main()
     //
     InitWindow(windowsWidth, windowsHeight, "Lukas's game!");
 
+    // accelaration due to gravity (pixel/frame)/frame
+    const int gravity{1};
+    const int jumpVel{-22};
+    bool isInAir(false);
+
     // rectangle dimenstions
     const int width(50);
     const int height(80);
 
     int posY(windowsHeight - height);
     int velocity{0};
+   
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -26,13 +32,29 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
-        if (IsKeyPressed(KEY_SPACE))
-        {
-            velocity -= 10;
-        }
-        
-        posY += velocity;
 
+        // ground check
+        if (posY >= windowsHeight - height)
+        {
+            // rectangle is on the ground
+            velocity = 0;
+            isInAir = false;
+        }
+        else 
+        {
+        // rectangle is in the air
+            velocity += gravity;
+            isInAir = true;
+
+        }
+        // check for jumping
+        if (IsKeyPressed(KEY_SPACE) && !isInAir)
+        {
+            velocity += jumpVel;
+        }
+        //update Y postion
+        posY += velocity;
+    
         DrawRectangle(windowsWidth/2, posY, width, height, BLUE);
         
         //Stop drawing
